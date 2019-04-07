f4cb62859d4d1bc252c490cea1b80f6385a5198d
mhco0 <mhco@cin.ufpe.br>
2019-04-07 00:38:01 -0300

    Adding project

---
+++ All-Methods/metodos.py
@@ -0,0 +1,28 @@
+def Adams_Moulton():
+	return
+def Adams_Bashforth():
+	return 
+def Euler():
+	return
+def Euler_Inverso():
+	return
+def Euler_Aprimorado():
+	return
+def Runge_Kutta():
+	return 
+def Formula_Inversa():
+	return
+
+def main():
+
+	
+	return 0
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
