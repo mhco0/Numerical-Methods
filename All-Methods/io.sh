d897bcb31169fc619a823aaeb054ec66579a3989
Marcos Heitor Carvalho de Oliveira <mhco@ug1c18.windows.cin.ufpe.br>
2019-04-11 16:12:33 -0300

    all right

---
+++ All-Methods/io.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source RUNME
+python3 metodos.py < entrega.in > out
+
--- All-Methods/virEnv/bin/activate.fish
+++ All-Methods/virEnv/bin/activate.fish
@@ -56,7 +56,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/CIN/tmt2/Desktop/Numerical-Methods/All-Methods/virEnv"
+set -gx VIRTUAL_ENV "/home/CIN/mhco/Desktop/Numerical-Methods/All-Methods/virEnv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
